9a23220c59912e66608d8e7e51c6496fb9d01459
Juancho <iralajua099@gmail.com>
2025-02-04 14:18:02 -0300

    Corregir rutas de carga de PHPMailer en sendMail.php

--- api/sendMail.php
+++ api/sendMail.php
@@ -7,9 +7,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 
-require __DIR__ . '/vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require __DIR__ . '/vendor/phpmailer/phpmailer/src/SMTP.php';
-require __DIR__ . '/vendor/phpmailer/phpmailer/src/Exception.php';
+require __DIR__ . './vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require __DIR__ . './vendor/phpmailer/phpmailer/src/SMTP.php';
+require __DIR__ . './vendor/phpmailer/phpmailer/src/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;

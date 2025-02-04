18cf6b61a1451a3e3a3c6134c7353875e3ad6183
Juancho <iralajua099@gmail.com>
2025-02-04 14:03:00 -0300

    Corregir la ruta del autoload de Composer en sendMail.php

--- api/sendMail.php
+++ api/sendMail.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Cargar el autoload de Composer
-require __DIR__ . 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
